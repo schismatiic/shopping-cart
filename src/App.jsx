2c2750c78e678c0b7dd562bea26ae3f5c14d6ad3
schismatiic <leonardojofre2003@gmail.com>
2026-07-31 20:38:21 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="bg-gray-700 justify-center">Hello</h1>
+    </>
+  );
+}
+
+export default App;
