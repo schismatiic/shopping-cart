99f9d9310df4699e27af2619e663ea45179be3b9
schismatiic <leonardojofre2003@gmail.com>
2026-08-02 11:21:11 -0400

    Hero changed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,14 +6,16 @@ const Hero = () => {
       className="flex justify-center text-center md:text-start flex-col text-slate-600 items-center md:items-start"
       id="hero"
     >
-      <h2 className="md:ml-18  font-heading font-black text-3xl">
-        Frilly pastel fashion for everyone
-      </h2>
-      <p className="md:ml-18">
-        Our designs are purrfect and range from XXS to 5X!
-      </p>
+      <div className="flex flex-col items-center md:items-start">
+        <h2 className="md:ml-18  font-heading font-black text-3xl md:text-5xl">
+          Frilly pastel fashion for everyone
+        </h2>
+        <p className="md:ml-18 text-lg md:text-xl font-bold">
+          Our designs are purrfect and range from XXS to 5X!
+        </p>
+      </div>
       <Link
-        className="md:ml-18 text-center cursor-pointer bg-pink-400 hover:bg-pink-500 text-teal-50 w-25 p-2 rounded-3xl font-bold"
+        className="md:ml-18 text-center border border-rose-300/30 cursor-pointer bg-pink-400 hover:bg-pink-500 text-teal-50 w-25 p-2 rounded-lg font-bold"
         to="/shop"
       >
         Shop now
