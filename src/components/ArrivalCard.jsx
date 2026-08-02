d609457b84ddab994f4c5e545f447248b0f241c9
schismatiic <leonardojofre2003@gmail.com>
2026-08-02 17:25:44 -0400

    Quick card fix

--- src/components/ArrivalCard.jsx
+++ src/components/ArrivalCard.jsx
@@ -3,22 +3,23 @@ import { Link } from "react-router";
 
 const ArrivalCard = ({ arrivalName, arrivalImage }) => {
   return (
-    <div className="flex h-full flex-col gap-2 border border-rose-300/30 rounded-xl overflow-hidden shadow-md">
+    <Link
+      to="/shop"
+      className="flex h-full flex-col gap-2 border border-rose-300/30 rounded-xl overflow-hidden shadow-md transition-transform duration-300 ease-out hover:scale-105 active:scale-95"
+    >
       <img
         className="w-full h-56 sm:h-64 lg:h-72 object-cover"
         src={arrivalImage}
         alt={arrivalName}
       />
       <div className="flex flex-1 flex-col gap-y-2 mx-4 my-3 text-slate-600 hover:text-slate-400 active:scale-95">
-        <Link to="/shop">
-          <h3 className="text-lg">{arrivalName}</h3>
-          <div className="mt-auto flex items-center cursor-pointer ">
-            <p className="text-sm">View collection</p>
-            <ArrowRight className="w-6 h-6" />
-          </div>
-        </Link>
+        <h3 className="text-lg">{arrivalName}</h3>
+        <div className="mt-auto flex items-center cursor-pointer ">
+          <p className="text-sm">View collection</p>
+          <ArrowRight className="w-6 h-6" />
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
--- src/components/FeaturedCard.jsx
+++ src/components/FeaturedCard.jsx
@@ -2,21 +2,22 @@ import { Link } from "react-router";
 
 const FeaturedCard = ({ featuredName, featuredImage, featuredPrice }) => {
   return (
-    <div className="flex h-full flex-col gap-2 border border-rose-300/30 rounded-xl overflow-hidden shadow-md">
+    <Link
+      to="/shop"
+      className="flex h-full flex-col gap-2 border border-rose-300/30 rounded-xl overflow-hidden shadow-md transition-transform duration-300 ease-out hover:scale-105 active:scale-95"
+    >
       <img
         className="w-full h-56 sm:h-64 lg:h-72 object-cover"
         src={featuredImage}
         alt={featuredName}
       />
       <div className="flex flex-1 flex-col gap-y-2 mx-4 my-3 text-slate-600 hover:text-slate-400 active:scale-95">
-        <Link to="/shop">
-          <h3 className="text-lg">{featuredName}</h3>
-          <div className="mt-auto flex items-center cursor-pointer ">
-            <p className="text-sm">${featuredPrice}</p>
-          </div>
-        </Link>
+        <h3 className="text-lg">{featuredName}</h3>
+        <div className="mt-auto flex items-center cursor-pointer ">
+          <p className="text-sm">${featuredPrice}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
